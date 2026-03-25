4f47476b597609d038aec67a4c0e3742c8ad20a2
Jorge Prado <100787833+jorgepradoh@users.noreply.github.com>
2026-03-24 18:22:49 -0600

    remove sitemap integration (crashes on build)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://jorgepradoh.github.io',
   integrations: [
     mdx(),
     react(),
-    sitemap(),
   ],
   markdown: {
     shikiConfig: {
